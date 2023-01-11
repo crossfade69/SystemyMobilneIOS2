69950278a03b196890c7e8c87f5feec35cd99299
crossfade69 <73494330+crossfade69@users.noreply.github.com>
2023-01-11 12:41:53 +0100

    Add files via upload

---
+++ SM9 - Multilanguage app/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SM9 - Multilanguage app
+//
+//  Created by Michał Napiórkowski on 21/12/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
